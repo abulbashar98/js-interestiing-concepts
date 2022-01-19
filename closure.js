ba3174a37645ebe27581540d48fe19dfcb5d1e0f
abulbashar98 <abulbashar98116@gmail.com>
2022-01-19 20:26:13 +0600

    closure-wesBoss

--- closure.js
+++ closure.js
@@ -1,6 +1,6 @@
+
+
 function stopWatch() {
-
-
 
     let counter = 0;
     return function () {
@@ -25,3 +25,62 @@ console.log(clock3())           //=====>1
 
 
 // Note: each variable of clock has created a closure for counter;
+
+
+
+
+
+/*-------------- *\
+    Wes Boss 
+\*-----------------*/
+
+// Closure
+function createGreeting(greeting = '') {
+    const myGreet = greeting.toUpperCase();
+    return function (name) {
+        return `${myGreet} ${name}`;
+    }
+}
+const sayHello = createGreeting('hello')
+const sayHey = createGreeting('hey')
+
+console.log(sayHello('wes'));
+console.log(sayHey('kait'))
+console.log(sayHello('wes'))
+
+
+
+// Note:  So closure is that we can use the greeting= '' by calling the inner function created as parameter sayHello, sayHey. Bcoz in Conventional sense after the createGreeting() function is done their parameters should go into garbage. but because of the inner function we can call that outer scope Parameter using closure again and again. Even after the higher scope function is closed.
+
+
+
+
+
+// Private Variable
+// Example 2:
+
+function createGame(gameName = '') {
+    let score = 0;
+    return function win() {
+        score++;
+        return `your ${gameName} score is ${score}`
+    }
+}
+
+const hockeyGame = createGame('hockey');
+const footballGame = createGame('football');
+
+console.log(hockeyGame())
+console.log(footballGame())
+console.log(hockeyGame())
+console.log(footballGame())
+console.log(hockeyGame())
+console.log(footballGame())
+console.log(hockeyGame())
+console.log(footballGame())
+
+
+// Note: we can see here now every time we call hockeyGame() or footballGame() now it it declares a variable score; And this private variable stores its value in closure;
+
+
+
